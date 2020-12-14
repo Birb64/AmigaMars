163619cb066ee9fb17f8bd6b6d0ccf2d1e12422c
Birb64 <60531532+Birb64@users.noreply.github.com>
2020-12-14 03:30:41 -0600

    I may have not gotten the jump to work right, but I got the rings working baby!

---
+++ AmigaMars/Assets/Models/rings/RingManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RingManager : MonoBehaviour
+{
+    public int rings;
+    public SpriteRenderer RingColumn1;
+    public SpriteRenderer RingColumn2;
+    public Sprite[] Sprites;
+    void Update()
+    {
+        RingColumn1.sprite = Sprites[rings % 10];
+        RingColumn2.sprite = Sprites[rings / 10];
+    }
+}
---
+++ AmigaMars/Assets/Models/rings/Rings.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rings : MonoBehaviour
+{
+    public int Amount;
+    public string tag;
+    bool HasActivated;
+    void Update()
+    {
+        if (HasActivated && !GetComponent<AudioSource>().isPlaying)
+        {
+            Destroy(gameObject);
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == tag)
+        other.gameObject.GetComponent<RingManager>().rings += Amount;
+        HasActivated = true;
+        GetComponent<AudioSource>().Play();
+    }
+}
